896ebf4b8e975c5729b3cd226c15e8c7831b33a8
Shad Storhaug <shad@shadstorhaug.com>
2025-05-01 22:46:55 +0700

    Update Main.java
    Range positive, power of two, nextInt(int, int)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+      java.util.Random r = new java.util.Random(42L);
+      
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < 64; i++)
+      {
+        int value = r.nextInt(100, 164);
+        // Convert to hexadecimal with 8 digits, padding with leading zeros if necessary
+        String hexValue = String.format("%08X", value);
+      
+        if (i > 0)
+        {
+          sb.append(", ");
+        }
+        sb.append("(int)0x");
+        sb.append(hexValue);
+      }
+    
+      System.out.println(sb.toString());
     }
 }
