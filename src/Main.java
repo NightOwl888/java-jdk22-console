adc0fbe34acc8794b4818dc0e36d272742a0f122
Shad Storhaug <shad@shadstorhaug.com>
2025-05-09 11:51:23 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,20 @@ public class Main {
       System.out.println("::group::Random Output");
       java.util.Random r = new java.util.Random(42L);
       
-      StringBuilder sb = new StringBuilder();
       for (int i = 0; i < 64; i++)
       {
         long value = r.nextLong(Integer.MIN_VALUE, Long.MAX_VALUE);
         // Convert to hexadecimal with 16 digits, padding with leading zeros if necessary
         String hexValue = String.format("%016X", value);
-      
-        if (i > 0)
-        {
-          sb.append(", ");
+
+        if (i > 0) {
+          System.out.print(" ");
         }
-        sb.append("(long)0x");
-        sb.append(hexValue);
+        System.out.print("(long)0x");
+        System.out.print(hexValue);
+        System.out.print(",");
       }
     
-      System.out.println(sb.toString());
       System.out.println("\n::endgroup::");
     }
 }
