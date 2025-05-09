b7e0d90289a4727d13e788abcc0b30d9bdc889d3
Shad Storhaug <shad@shadstorhaug.com>
2025-05-09 13:11:50 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
       
       for (int i = 0; i < 64; i++)
       {
-        long value = r.nextLong(100, 150);
+        int value = r.nextInt(64);
         // Convert to hexadecimal with 16 digits, padding with leading zeros if necessary
-        String hexValue = String.format("%016X", value);
+        String hexValue = String.format("%08X", value);
 
         if (i > 0) {
           System.out.print(" ");
         }
-        System.out.print("(long)0x");
+        System.out.print("(int)0x");
         System.out.print(hexValue);
         System.out.print(",");
       }
