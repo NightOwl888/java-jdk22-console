9479240d2598d9351366c45006993aed5d828e80
Shad Storhaug <shad@shadstorhaug.com>
2025-05-09 11:47:16 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ public class Main {
       StringBuilder sb = new StringBuilder();
       for (int i = 0; i < 64; i++)
       {
-        int value = r.nextInt(100, 150);
-        // Convert to hexadecimal with 8 digits, padding with leading zeros if necessary
-        String hexValue = String.format("%08X", value);
+        long value = r.nextLong(Integer.MIN_VALUE, Long.MAX_VALUE);
+        // Convert to hexadecimal with 16 digits, padding with leading zeros if necessary
+        String hexValue = String.format("%016X", value);
       
         if (i > 0)
         {
           sb.append(", ");
         }
-        sb.append("(int)0x");
+        sb.append("(long)0x");
         sb.append(hexValue);
       }
     
